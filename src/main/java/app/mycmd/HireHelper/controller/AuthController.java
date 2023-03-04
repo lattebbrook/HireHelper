418512f115975cb5d8067487a2b8c3d81102b993
Tanawat Boonmak <tanawat.brook@icloud.com>
2023-03-04 17:22:08 +0700

    Adding thymeleaf dependency
    Adding thymeleaf dependency version 3.0.2

--- src/main/java/app/mycmd/HireHelper/controller/AuthController.java
+++ src/main/java/app/mycmd/HireHelper/controller/AuthController.java
@@ -1,5 +1,16 @@
 package app.mycmd.HireHelper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthController {
+    
+    public String login(HttpServletRequest request, @RequestParam String keyapi) {
+        
+        return "access-denied.html";
+    }
+            
     
 }
